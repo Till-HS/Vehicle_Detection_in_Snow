988713046629fe8a8bfaed95e5bfb4bff816ed70
Till Schwiers <till.schwiers@tuhh.de>
2025-04-17 13:40:51 +0200

    added scripts for setup of vm

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+mkdir project
+cd project
+mkdir data
+cd data
+
+curl "https://filedn.eu/lwnF6OGJjyNyTUCGJUbsnVp/public_data/project_nvd/Labeled%20Frames%20(YOLO%20Format).zip" --output labeled_frames_YOLO_format.zip
+unzip *.zip
