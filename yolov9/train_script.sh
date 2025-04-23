d9300121857e0fda852642f1bcd0481c6121521b
Till Schwiers <till.schwiers@tuhh.de>
2025-04-23 10:19:51 +0200

    new file structure

--- yolov9/train_script.sh
+++ yolov9/train_script.sh
@@ -1,6 +1,6 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-python train_dual.py \
+python yolov9/train_dual.py \
 --batch 1 --epochs 25 --imgsz 1920 --device 0 --min-items 0 --close-mosaic 15 \
 --data data/dataset.yaml \
 --weights data/weights/gelan-c.pt \
